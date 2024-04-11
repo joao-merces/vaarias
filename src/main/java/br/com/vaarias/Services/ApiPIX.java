5ef1d51cb5ae8d6a72d29b2b0ed3a57bd9e5d670
joao-merces <victor.joao.rm@gmail.com>
2024-04-10 22:13:58 -0300

    recommended books

---
+++ src/main/java/br/com/vaarias/Services/ApiPIX.java
@@ -0,0 +1,39 @@
+package br.com.vaarias.Services;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiPIX {
+    private String apiUrl;
+
+    public ApiPIX(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
+
+    public Image getQRCode() {
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedImage bufferedImage = ImageIO.read(connection.getInputStream());
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                return image;
+            } else {
+                System.out.println("Failed to fetch image. Response code: " + responseCode);
+                return null;
+            }
+        } catch (IOException e) {
+            System.out.println("Error fetching image: " + e.getMessage());
+            return null;
+        }
+    }
+}
